360d6118b4029ee5db7aea4f87f16e6772945d6c
Sai Nithin <jagannadhasainithinkommineni@gmail.com>
2025-07-31 23:45:46 +0530

    Add TrainerModule to calculate salary with student reviews

---
+++ OppsConcept/TrainerModule.java
@@ -0,0 +1,58 @@
+package OppsConcept;
+
+import java.util.Scanner;
+
+public class TrainerModule {
+    int trainerId;
+    String trainerName;
+    int totalSections;
+    int perSection = 1500;
+    int totalslary;
+    Scanner sc =new Scanner(System.in);
+    public void greetTrainer(){
+        System.out.println("Enter Trainer ID:");
+        trainerId = sc.nextInt();
+        System.out.println("Enter Trainer Name:");
+        trainerName = sc.next();
+
+    }
+    public int calculateSalary(){
+        System.out.println("Enter Total Sections Handled:");
+        totalSections = sc.nextInt();
+        totalslary = totalSections*perSection;
+        StudentModule studentReview = new StudentModule();
+        byte review = studentReview.studentRewiew;
+        switch (review) {
+            case 5:
+                totalslary += 5000;
+                
+                break;
+            case 4:
+                totalslary += 4000;
+                break;
+            case 3:
+                totalslary += 3000;
+                break;
+            case 2:
+                totalslary += 2000;
+                break;
+            case 1:
+                totalslary += 1000;
+                break;
+            
+
+            default:
+                totalslary +=0;
+                break;
+        }
+        return totalslary;
+
+    }
+    public void TrainerReview(){
+        System.out.println("Trainer ID:"+ trainerId);
+        System.out.println("Trainer Name:"+ trainerName);
+        System.out.println("Total Section Handled: "+ totalSections);
+        System.out.println("Total salary"+ calculateSalary());
+    }
+
+}
