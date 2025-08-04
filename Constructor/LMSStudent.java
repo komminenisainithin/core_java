cd433f131e761e4bbde834b312a8592424a7218f
Sai Nithin <jagannadhasainithinkommineni@gmail.com>
2025-08-04 23:21:08 +0530

    Implemented LMS Student Tracker with: - Constructor overloading (default, ID & name, full details) - Fee discount calculation based on grade - Input handling via constructor - Fee payment status check and display methods

---
+++ Constructor/LMSStudent.java
@@ -0,0 +1,10 @@
+package Constructor;
+
+public class LMSStudent {
+    public static void main(String[] args) {
+        Student stu =new Student();
+        stu.display();
+        stu.feesStatus();
+    }
+
+}
